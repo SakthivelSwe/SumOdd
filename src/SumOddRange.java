5ee622b411fdd85613c8357311bedf32a4ee82c6
sakthiveljoker <77488929+sakthiveljoker@users.noreply.github.com>
2021-08-08 21:43:49 +0530

    First commit

---
+++ src/SumOddRange.java
@@ -0,0 +1,24 @@
+public class SumOddRange {
+
+    public static boolean isOdd(int number) {
+        if(number < 0) {
+            return false;
+        }else return number % 2 != 0;
+    }
+
+    public static int sumOdd(int start, int end) {
+        int sum = 0;
+        if(end < start || start < 0) {
+            return -1;
+        }else {
+            for(int i =start;i<=end;i++) {
+                if(isOdd(i)) {
+                    sum +=i;
+                    System.out.println("odd number= "+i);
+                }
+
+            }
+           return sum;
+        }
+    }
+}
